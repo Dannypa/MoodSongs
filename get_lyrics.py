c4b43cffb2b16eb789d805fdf581fe416d39dd8c
Cosmoden <dennis.lavrushin@gmail.com>
2022-05-25 12:21:27 +0300

    downloaded some lyrics into the database

--- get_lyrics.py
+++ get_lyrics.py
@@ -1,20 +1,44 @@
 from csv import DictReader
 import sqlite3
+from bs4 import BeautifulSoup
+import requests
+import re
+import logging
 
-with open("tcc_ceds_music.csv", encoding='utf-8', mode='r') as f:
+logging.basicConfig(filename='get_lyrics.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
+
+
+def scrape_lyrics(artistname, songname):
+    artistname2 = str(artistname.replace(' ', '-')) if ' ' in artistname else str(artistname)
+    songname2 = str(songname.replace(' ', '-')) if ' ' in songname else str(songname)
+    page = requests.get('https://genius.com/' + artistname2 + '-' + songname2 + '-' + 'lyrics')
+    html = BeautifulSoup(page.text, 'html.parser')
+    lyrics = html.find("div", class_=re.compile("Lyrics"))
+    try:
+        return lyrics.get_text()
+    except AttributeError:
+        logging.error(f"No text for {artistname} - {songname}")
+        return ""
+
+
+with open("SongCSV.csv", encoding='utf-8', mode='r') as f:
     reader = DictReader(f, delimiter=',')
     con = sqlite3.connect("lyrics.db")
     cur = con.cursor()
+    cur.execute("DELETE FROM lyrics WHERE TRUE")
+    con.commit()
     for i, line in enumerate(reader):
-        artist = line["artist_name"].replace('\'', '').replace('"', '')
-        name = line["track_name"].replace('\'', '').replace('"', '')
-        text = line["lyrics"].replace('\'', '').replace('"', '')
+        if i < 1000:
+            continue
+        if i == 2000:
+            break
+        artist = line["ArtistName"][1:-1].replace('\'', '').replace('"', '')
+        name = line["Title"][1:-1].replace('\'', '').replace('"', '')
+        if name.split()[-1][0] == '(' and name.split()[-1][-1] == ')':
+            name = ' '.join(name.split()[:-1])
+        text = scrape_lyrics(artist, name).replace('\'', '').replace('"', '')
         cmd = f"""INSERT INTO lyrics (artist, name, text)
                   VALUES ('{artist}', '{name}', '{text}')"""
-        try:
-            cur.execute(cmd)
-        except sqlite3.OperationalError as e:
-            print(str(e))
-            exit(0)
-    con.commit()
+        cur.execute(cmd)
+        con.commit()
     con.close()
